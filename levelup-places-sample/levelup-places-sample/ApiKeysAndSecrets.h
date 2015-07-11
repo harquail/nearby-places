4c9148fa21618573224e8091bdea79635f54fcba
harquail <nook@harquail.com>
2015-07-11 18:39:04 -0400

    added google places pod + API key

---
+++ levelup-places-sample/levelup-places-sample/ApiKeysAndSecrets.h
@@ -0,0 +1,6 @@
+// ApiKeysAndSecrets.h
+//
+//  Created by nook on 7/11/15.
+//  Copyright (c) 2015 nook. All rights reserved.
+//
+#define kGooglePlacesAPIKey @"AIzaSyCpWlndIhuOtDaU2UPgXiqIF4ty7w1KJV0"
