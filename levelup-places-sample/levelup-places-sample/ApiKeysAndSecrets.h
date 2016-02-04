48016f039c42da3ae51d23d5d9cda692ba74770e
harquail <nook@harquail.com>
2016-02-04 11:36:27 -0500

    note that api key has been deleted

--- levelup-places-sample/levelup-places-sample/ApiKeysAndSecrets.h
+++ levelup-places-sample/levelup-places-sample/ApiKeysAndSecrets.h
@@ -3,4 +3,5 @@
 //  Created by nook on 7/11/15.
 //  Copyright (c) 2015 nook. All rights reserved.
 //
+// this key has been disabled — generate a new key here: https://console.developers.google.com/
 #define kGooglePlacesAPIKey @"AIzaSyAs2zeFyJemFSNSlXSOz07fzLRWmSa1Weg"
